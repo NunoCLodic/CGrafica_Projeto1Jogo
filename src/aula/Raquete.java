ed9ab464ad96bf71a6ce259edbe6c2c388ac4b3b
NunoClodic Carvalho Lima <32763809+NunoCLodic@users.noreply.github.com>
2018-09-18 16:07:38 -0300

    At1
    Atualizacao

---
+++ src/aula/Raquete.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula;
+
+import java.awt.Graphics;
+
+/**
+ *
+ * @author glauc
+ */
+public class Raquete extends Base{
+
+    @Override
+    public void desenhar(Graphics g) {
+        g.setColor(cor);
+        g.fillRect(x, y, largura, altura);
+        g.setColor(borda);
+        g.drawRect(x, y, largura, altura);
+    }
+    
+}
